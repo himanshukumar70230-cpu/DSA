a47161bc9cb9e94ab157f6fb3fbf11c3676d7f95
Himanshu kumar <himanshukumar70230@gmail.com>
2026-09-01 21:57:00 +0530

    Time: 0 ms (100%), Space: 41.2 MB (41.51%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -4,15 +4,14 @@ public:
         int n = nums.size();
         vector<int>pre(n);
         vector<int>suf(n);
-        pre[0] = 1;
-        //calculating the pre-product of array elements
-        for(int i= 1;i<n;i++){
-            pre[i] = nums[i-1]*pre[i-1];
+        pre[0] =1;
+        suf[n-1] = 1;
+        for(int i = 1;i<n;i++){
+            pre[i] = nums[i-1] *pre[i-1];
         }
 
-        suf[n-1] = 1;
         for(int i = n-2;i>=0;i--){
-            suf[i] = suf[i+1]*nums[i+1];
+            suf[i] = nums[i+1]*suf[i+1]; 
         }
 
         for(int i = 0;i<n;i++){
