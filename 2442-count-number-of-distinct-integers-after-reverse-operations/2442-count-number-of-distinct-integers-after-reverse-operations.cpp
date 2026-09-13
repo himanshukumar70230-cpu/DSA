dccd2158c24e675b40eb78580d92c14c74ac6baa
Himanshu kumar <himanshukumar70230@gmail.com>
2026-09-13 18:41:25 +0530

    Time: 177 ms (69.45%), Space: 110.6 MB (53.55%) - LeetHub

--- 2442-count-number-of-distinct-integers-after-reverse-operations/2442-count-number-of-distinct-integers-after-reverse-operations.cpp
+++ 2442-count-number-of-distinct-integers-after-reverse-operations/2442-count-number-of-distinct-integers-after-reverse-operations.cpp
@@ -1,25 +1,21 @@
 class Solution {
 public:
 int reverse(int n){
-    int rev = 0,rem;
+    int rem = 0, rev = 0;
     while(n>0){
         rem = n%10;
         rev = rev*10+rem;
-        n/=10;
+        n = n/10;
     }
     return rev;
 }
     int countDistinctIntegers(vector<int>& nums) {
-        int n = nums.size();
-        unordered_set<int>s;
-        for(int i = 0;i<n;i++){
-            s.insert(nums[i]);
-        }
-        for(int i = 0;i<n;i++){
-            int rev = reverse(nums[i]);
-            s.insert(rev);
-        }
-        return s.size();
-
+       unordered_set<int>s;
+       for(int  i = 0; i<nums.size(); i++){
+        s.insert(nums[i]);
+        int rev = reverse(nums[i]);
+        s.insert(rev);
+       }
+       return s.size(); 
     }
 };
