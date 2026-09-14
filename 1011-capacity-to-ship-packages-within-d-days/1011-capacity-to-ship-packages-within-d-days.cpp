4247b52d27ae0734415fa69abe4e895f9ee0b5ca
Himanshu kumar <himanshukumar70230@gmail.com>
2026-09-14 16:51:47 +0530

    Time: 8 ms (82.36%), Space: 35 MB (54.36%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
@@ -1,41 +1,36 @@
 class Solution {
 public:
-bool helper(vector<int>& arr,int days,int mc){
-    int n = arr.size();
-    int r = mc;
-    int cDays = 1;
-    for(int i = 0;i<n;i++){
-        if(r>=arr[i]){
-            r-=arr[i];
+bool check(vector<int>& weights, int days,int mid){
+    int count  = 1;
+    int m = mid;
+    for(int i = 0; i<weights.size(); i++){
+        if(m>=weights[i]){
+            m-=weights[i];
         }else{
-            cDays++;
-            r = mc;
-            r-=arr[i];
+            count++;
+            m = mid;
+            m -=weights[i];
         }
     }
-    
-  
-    if(cDays>days) return false;
-    return true;
-
+    if(count <= days) return true;
+    return false;
 }
     int shipWithinDays(vector<int>& weights, int days) {
-        int n = weights.size();
-        int minCapacity = INT_MIN;
-        int maxCapacity = 0;
-        for(int i = 0;i<n;i++){
-            if(weights[i]>minCapacity) minCapacity = weights[i];
-            maxCapacity+=weights[i];
+        int ans;
+        int lo = INT_MIN;
+        int hi = 0;
+        for(int i = 0;i<weights.size();i++){
+            if(weights[i]>lo) lo = weights[i];
+            hi+=weights[i];
         }
-        int lo = minCapacity;
-        int hi = maxCapacity;
-        int ans = 0;
+        cout<<lo<<" "<<hi<<endl;
         while(lo<=hi){
-            int mid = lo +(hi-lo)/2;
-            if(helper(weights,days,mid) == true){
+            int mid = lo+(hi-lo)/2;
+            if(check(weights,days,mid) == true){
                 ans = mid;
                 hi = mid-1;
-            }else lo = mid+1;
+                }
+            else lo = mid+1;
         }
         return ans;
     }
